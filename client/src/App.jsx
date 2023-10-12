877672e6e560858c9fcced1e39591b33dec94aa5
Sumit <sumitpadalkar53@gmail.com>
2023-10-12 06:55:36 +0530

    Intial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from './component/Login'
+
+export default function App() {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  )
+}
---
+++ client/src/component/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Login(){
+
+    return(
+      <div>Login</div>
+    )
+}
